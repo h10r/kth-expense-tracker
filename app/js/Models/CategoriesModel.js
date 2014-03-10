1ba6214a431ead3748f08866ba6a181bdf518976
Simon Kreiser <kreiser@idontmind.de>
2014-03-10 11:59:03 +0100

    models cleaned up and added default categories

--- app/js/Models/CategoriesModel.js
+++ app/js/Models/CategoriesModel.js
@@ -1,25 +1,54 @@
 //CategoryModel Object constructor
-expenseTrackerAppModule.service("CategoriesModel", function() {
+expenseTrackerAppModule.service('CategoriesModel', function () {
+	'use strict';
 
-	categories = [];
+	var categories = [],
+		category = {
+			id : '',
+			title : '',
+			color : '',
+			icon : ''
+		};
 
-	var category = {
-		id : "",
-		title : "",
-		color : ""
-	};
+	// default categories
+	categories.push(
+		{
+			id : '1',
+			title : 'groceries',
+			color : '#4c9533',
+			icon : ''
+		},
+		{
+			id : '2',
+			title : 'eating out',
+			color : '#2781c0',
+			icon : ''
+		},
+		{
+			id : '3',
+			title : 'coffee',
+			color : '#c07327',
+			icon : ''
+		},
+		{
+			id : '4',
+			title : 'beer',
+			color : '#ecd84f',
+			icon : ''
+		}
+	);
 
 	return {
 		addCategory : function(category) {
 			// return detailViewId;
 		},
 
-		removeCategory : function(category) {
+		removeCategory : function(categoryId) {
 			// return detailViewId;
 		},
 
 		getCategoryById : function(categoryId) {
 			// return detailViewId;
 		}
-	}
+	};
 });
