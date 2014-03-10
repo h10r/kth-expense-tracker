79ec7cfd9ed88af0775f4b2dc9f8781e1932cdf7
Simon Kreiser <kreiser@idontmind.de>
2014-03-10 13:05:16 +0100

    small link fix

--- app/js/app.js
+++ app/js/app.js
@@ -6,8 +6,8 @@ var expenseTrackerAppModule = angular.module('expenseTracker', ['ngRoute']);
 expenseTrackerAppModule.config(['$routeProvider', function ($routeProvider) {
 
   // main navigation
-  $routeProvider.when('/expenses/add', {templateUrl: 'partials/expense/expense-add.html', controller: 'expenseTracker.ExpenseController'});
-  $routeProvider.when('/expenses/add/category', {templateUrl: 'partials/expense/expense-add-category.html', controller: 'expenseTracker.ExpenseController'});
+  $routeProvider.when('/expenses/add', {templateUrl: 'partials/expense/expense-add.html', controller: 'expenseTracker.ExpenseAddController'});
+  $routeProvider.when('/expenses/add/category', {templateUrl: 'partials/expense/expense-add-category.html', controller: 'expenseTracker.ExpenseAddController'});
   $routeProvider.when('/feeds', {templateUrl: 'partials/feeds/feeds.html', controller: 'expenseTracker.FeedController'});
   $routeProvider.when('/overview', {templateUrl: 'partials/overview/overview.html', controller: 'expenseTracker.OverviewController'});
   $routeProvider.when('/settings', {templateUrl: 'partials/settings/settings.html', controller: 'expenseTracker.SettingsController'});
