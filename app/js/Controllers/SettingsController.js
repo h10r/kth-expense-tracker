5638f99e800447ff608f8a2df6ff3643ef77714a
hendrik <hendrikheuer@gmail.com>
2014-03-13 15:23:38 +0100

    adding budget works

--- app/js/Controllers/SettingsController.js
+++ app/js/Controllers/SettingsController.js
@@ -1,3 +1,14 @@
 expenseTrackerAppModule.controller('expenseTracker.SettingsController', function($scope, $rootScope, UserModel) {
 
+	$scope.currentUser = UserModel.getCurrentUser();
+
+	$scope.addBudget = function() {
+
+		console.log("saveBudget");
+		//UserModel.addBudget();
+		console.log("addBudget.value");
+
+		//$location.path('');
+	}
+
 });
